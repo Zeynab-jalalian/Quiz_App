29f09a49e1ee638b10afb9a1ab8ae1a0ec63e40e
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-11-06 10:31:02 +0330

    quizdata

---
+++ script.js
@@ -0,0 +1,42 @@
+const quizData = [
+  {
+    question: "Which artwork is known for its mysterious smile?",
+    a: "The Last Supper",
+    b: "The Birth of Venus",
+    c: "Mona Lisa",
+    d: "Girl with a Pearl Earring",
+    correct: "c"
+  },
+  {
+    question: "Which painting depicts melting clocks in a surreal landscape?",
+    a: "The Scream",
+    b: "The Persistence of Memory",
+    c: "Guernica",
+    d: "The Garden of Earthly Delights",
+    correct: "b"
+  },
+  {
+    question: "Which artwork was painted by Edvard Munch?",
+    a: "The Kiss",
+    b: "American Gothic",
+    c: "The Starry Night",
+    d: "The Scream",
+    correct: "d"
+  },
+  {
+    question: "Who created the artwork 'Girl with a Pearl Earring'?",
+    a: "Vermeer",
+    b: "Rembrandt",
+    c: "Picasso",
+    d: "Da Vinci",
+    correct: "a"
+  },
+  {
+    question: "Which artwork is a large black-and-white mural portraying the tragedy of war?",
+    a: "Guernica",
+    b: "The Night Watch",
+    c: "The Creation of Adam",
+    d: "Nighthawks",
+    correct: "a"
+  }
+];
