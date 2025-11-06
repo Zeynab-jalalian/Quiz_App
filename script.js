38b3084005434780ba183983109c712c77823a03
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-11-06 11:03:18 +0330

    js...

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const quizData = [
     b: "The Birth of Venus",
     c: "Mona Lisa",
     d: "Girl with a Pearl Earring",
-    correct: "c"
+    correct: "c",
   },
   {
     question: "Which painting depicts melting clocks in a surreal landscape?",
@@ -13,7 +13,7 @@ const quizData = [
     b: "The Persistence of Memory",
     c: "Guernica",
     d: "The Garden of Earthly Delights",
-    correct: "b"
+    correct: "b",
   },
   {
     question: "Which artwork was painted by Edvard Munch?",
@@ -21,7 +21,7 @@ const quizData = [
     b: "American Gothic",
     c: "The Starry Night",
     d: "The Scream",
-    correct: "d"
+    correct: "d",
   },
   {
     question: "Who created the artwork 'Girl with a Pearl Earring'?",
@@ -29,14 +29,37 @@ const quizData = [
     b: "Rembrandt",
     c: "Picasso",
     d: "Da Vinci",
-    correct: "a"
+    correct: "a",
   },
   {
-    question: "Which artwork is a large black-and-white mural portraying the tragedy of war?",
+    question:
+      "Which artwork is a large black-and-white mural portraying the tragedy of war?",
     a: "Guernica",
     b: "The Night Watch",
     c: "The Creation of Adam",
     d: "Nighthawks",
-    correct: "a"
-  }
+    correct: "a",
+  },
 ];
+
+const questionEl = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+
+let currentQuiz = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+  const currentQuizData = quizData[currentQuiz];
+  
+  questionEl.innerText = currentQuizData.question;
+  a_text.innerText = currentQuizData.a;
+  b_text.innerText = currentQuizData.b;
+  c_text.innerText = currentQuizData.c;
+  d_text.innerText = currentQuizData.d;
+
+  currentQuiz++;
+}
