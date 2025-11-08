1e2556a14af5212f4c536647600e497875f80a8a
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-11-08 20:07:06 +0330

    js..

--- script.js
+++ script.js
@@ -47,19 +47,44 @@ const a_text = document.getElementById("a_text");
 const b_text = document.getElementById("b_text");
 const c_text = document.getElementById("c_text");
 const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
 
 let currentQuiz = 0;
+let score = 0;
 
 loadQuiz();
 
 function loadQuiz() {
   const currentQuizData = quizData[currentQuiz];
-  
+
   questionEl.innerText = currentQuizData.question;
   a_text.innerText = currentQuizData.a;
   b_text.innerText = currentQuizData.b;
   c_text.innerText = currentQuizData.c;
   d_text.innerText = currentQuizData.d;
-
-  currentQuiz++;
 }
+
+function getSelected() {
+  const answerEls = document.querySelectorAll(".answer");
+  answerEls.forEach((answerEl) => {
+    if (answerEl.checked) {
+      return answerEl.id;
+    }
+  });
+  return undefined;
+}
+
+submitBtn.addEventListener("click", () => {
+  const answer = getSelected();
+  if (answer) {
+    if (answer === quizData[currentQuiz]) {
+    }
+  }
+  currentQuiz++;
+
+  if (currentQuiz < quizData.length) {
+    loadQuiz();
+  } else {
+    alert("You finished! Get yourself a Orange Lemonda");
+  }
+});
