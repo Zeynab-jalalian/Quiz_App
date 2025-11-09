6e48c0698adeb0c7fb13dff4623ab2b537d8ee17
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-11-09 20:19:14 +0330

    fix

--- script.js
+++ script.js
@@ -86,19 +86,17 @@ function deselectAnswers() {
 submitBtn.addEventListener("click", () => {
   const answer = getSelected();
   if (answer) {
-    if (answer) {
-      if (answer === quizData[currentQuiz].correct) {
-        score++;
-      }
-      currentQuiz++;
+    if (answer === quizData[currentQuiz].correct) {
+      score++;
+    }
+    currentQuiz++;
 
-      if (currentQuiz < quizData.length) {
-        loadQuiz();
-      } else {
-        quiz.innerHTML = `<h2>You answered correctly at ${score}/${quizData.length} questions.</h2>
+    if (currentQuiz < quizData.length) {
+      loadQuiz();
+    } else {
+      quiz.innerHTML = `<h2>You answered correctly at ${score}/${quizData.length} questions.</h2>
          <button onclick="location.reload()">Reload</button>
          `;
-      }
     }
   }
 });
